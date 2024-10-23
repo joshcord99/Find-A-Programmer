c2f077a94c2f8ac613027d198194de19e1b03afd
joshcord99 <joshcord99@gmail.com>
2024-10-23 01:20:50 -0700

    added nav buttons

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,7 +1,16 @@
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <>
+
+    <button>
+      Home
+      </button>  
+    <button>
+     Potential Candidates
+      </button>
+
+    </>
   )
 };
 

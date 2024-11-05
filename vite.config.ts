69135ec493360dda684c4b11f566efca0d7c6a8d
Joshua Cordial <joshcord99@gmail.com>
2024-11-04 21:20:54 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  envDir: '',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 3000,
+  },
 });
